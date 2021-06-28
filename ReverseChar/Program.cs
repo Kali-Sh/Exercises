9c7a4676e52d2936bb4eb1b3ae18f986dbf0d5f1
Kali-Sh <kalbro721@gmail.com>
2021-06-28 10:54:20 +0300

    Initial

---
+++ ReverseChar/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ReverseChar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input=> ");
+            string input = Console.ReadLine();
+            string output = "";
+
+            for (int i = input.Length - 1; i >= 0; i--)
+            {
+                output += input[i];
+            }
+
+            Console.WriteLine($"Output=> {output}");
+        }
+    }
+}
