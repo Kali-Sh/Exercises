c33c3adfcbc2523b4402bb5e76dbf867511fc83b
Kali-Sh <kalbro721@gmail.com>
2021-06-28 11:03:57 +0300

    Initial

---
+++ RemoveChar/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace RemoveChar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int counter = 0;
+
+            for (int i = 0; i < input.Length - 1; i++)
+            {
+                counter = 0;
+                for (int j = i + 1; j < input.Length; j++)
+                {
+                    if (input[i] == input[j])
+                    {
+                        counter++;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                if (counter != 0)
+                {
+                    input = input.Remove(i, counter);
+                }
+
+            }
+            Console.WriteLine(input);
+        }
+    }
+}
